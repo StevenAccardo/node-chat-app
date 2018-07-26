c84c0c21051611e1bd2a11c18dd92c96ace2bafd
Steven Accardo <accardo.steven@gmail.com>
2018-07-25 19:16:25 -0700

    Added an auto response for a new user entering chat

--- server/index.js
+++ server/index.js
@@ -1,10 +1,46 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../client');
 const port = process.env.PORT || 3000;
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'A new user has joined!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.on('createMessage', message => {
+    console.log('createMessage', message);
+    // io.emit('newMessage', {
+    //   from: newMessage.from,
+    //   text: newMessage.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.broadcast.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () => console.log('The user disconnected'));
+});
+
+server.listen(port, () => console.log(`Server is listening on port ${port}`));
